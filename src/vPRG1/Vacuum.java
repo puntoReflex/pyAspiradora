1e7736d434fb3374ba28038239cae19bec653b1c
manuel@sdf1.fedora <manuel@masiasweb.com>
2026-03-08 01:12:20 +0100

    feat: se dota a la aspiradora de autogestión de recursos (batería autoconsciente y bolsa mutable)

--- src/vPRG1/Vacuum.java
+++ src/vPRG1/Vacuum.java
@@ -27,20 +27,17 @@ class Vacuum {
         };
         int vacuumPosition[] = { 0, 0 };
 
-        int battery = 5 * surface.length * surface[0].length;
+        int[] battery = { 5 * surface.length * surface[0].length, 5 * surface.length * surface[0].length };
         int[] bag = { 0, 3 * surface.length * surface[0].length };
 
         contaminate(surface);
         boolean surfaceIsDirty = true;
-        boolean hasBattery = battery > 0;
 
-        while (surfaceIsDirty && hasBattery) {
+        while (surfaceIsDirty) {
             moveVacuum(vacuumPosition, surface);
-            cleanWorld(surface, vacuumPosition, bag);
-            battery--;
+            cleanWorld(surface, vacuumPosition, bag, battery);
             printWorld(surface, vacuumPosition, battery, bag);
             surfaceIsDirty = isDirty(surface);
-            hasBattery = battery > 0;
             pause(0.5);
         }
     }
@@ -49,6 +46,12 @@ class Vacuum {
         System.out.println("Bolsa llena. Presione ENTER para vaciar...");
         new Scanner(System.in).nextLine();
         bag[0] = 0;
+    }
+
+    static void rechargeBattery(int[] battery) {
+        System.out.println("Batería agotada. Presione ENTER para recargar...");
+        new Scanner(System.in).nextLine();
+        battery[0] = battery[1];
     }
 
     static void moveVacuum(int[] vacuumPosition, int[][] surface) {
@@ -76,12 +79,16 @@ class Vacuum {
                 position[1] < 0 || position[1] >= surface[0].length;
     }
 
-    static void cleanWorld(int[][] surface, int[] vacuumPosition, int[] bag) {
+    static void cleanWorld(int[][] surface, int[] vacuumPosition, int[] bag, int[] battery) {
         if (surface[vacuumPosition[0]][vacuumPosition[1]] > 0) {
             surface[vacuumPosition[0]][vacuumPosition[1]]--;
             bag[0]++;
+            battery[0]--;
             if (bag[0] >= bag[1]) {
                 emptyBag(bag);
+            }
+            if (battery[0] <= 0) {
+                rechargeBattery(battery);
             }
         }
     }
@@ -96,7 +103,7 @@ class Vacuum {
         return false;
     }
 
-    static void printWorld(int[][] aMap, int[] vacuumPosition, int battery, int[] bag) {
+    static void printWorld(int[][] aMap, int[] vacuumPosition, int[] battery, int[] bag) {
         cleanScreen();
         System.out.println("---".repeat(aMap[0].length));
         for (int row = 0; row < aMap.length; row++) {
@@ -110,7 +117,7 @@ class Vacuum {
             System.out.println();
         }
         System.out.println("---".repeat(aMap[0].length));
-        System.out.println("Batería: " + battery + " | Bolsa: " + bag[0] + "/" + bag[1]);
+        System.out.println("Batería: " + battery[0] + "/" + battery[1] + " | Bolsa: " + bag[0] + "/" + bag[1]);
     }
 
     static String mapVacuum() {
